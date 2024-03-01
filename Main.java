e88485da647fbba8a9d009a5542d8e3480bdfc5e
Den090706 <137331608+Den090706@users.noreply.github.com>
2024-03-01 12:16:30 +0000

    javadoc-private - d doc *.java

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
+/** 
+* print comand-line parameters. 
+*/
+
 public class Main{
+  /**
+   * Program entry point
+   * @param args comand comand-line parameters list
+   */
   public static void main(String[] args){
     for (String s: args){
       System.out.println(s);
