555cb7367ce8729a6ce4b3996301776ed2d5b0fb
Den090706 <137331608+Den090706@users.noreply.github.com>
2024-03-03 21:33:36 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
-public class Main{
-  /**
-   * Program entry point
-   * @param args comand comand-line parameters list
-   */
-  public static void main(String[] args){
-    for (String s: args){
-      System.out.println(s);
+public class Main {
+    public static void main(String[] args) {
+        String[] months = {"Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"};
+
+        for (String month : months) {
+            System.out.println(month);
+        }
     }
-  }
 }
